8b0f331889ae62a61c29e2fac0bf2bb9bc34e992
Mauricio Alarcon <mauricio.alarcon@coxautoinc.com>
2019-10-25 17:37:11 -0400

    CEPC08B skyscrapers running

--- spoj/CEPC08B_skyscrapers.py
+++ spoj/CEPC08B_skyscrapers.py
@@ -2,20 +2,13 @@ from bisect import bisect_left
 height = 0
 def findSky (st, fin):
     if (fin - st < 2):
-        if (sortedSky[st]["height"] == height):
-            return st
-        else:
-            return -1
+        return st
     mid = st + ((fin - st) // 2)
     if (sortedSky[mid]["height"] == height):
         return mid
-    first = findSky(st, mid)
-    if (first != -1):
-        return first
-    second = findSky(mid + 1, fin)
-    if (second != -1):
-        return second
-    return -1
+    elif (sortedSky[mid]["height"] < height):
+        return findSky(mid + 1, fin)
+    return findSky(st, mid)
 
 def findFirstSky (h, n):
     global height
@@ -34,23 +27,38 @@ tests = int(input())
 results = [0]*tests
 for test in range(tests):
     n_sky, n_days = [int(x) for x in input().split()]
+    newLand = [False]*n_sky
+    groupList = [0]*n_days
+    groups = 0
+
     skyscrapers = [{"height": int(sky), "pos": ind} for ind, sky in enumerate(input().split(), start=0)]
+    days = [{"height": -int(day), "pos": ind} for ind, day in enumerate(input().split(), start=0)]
+
     sortedSky = sorted(skyscrapers, key=lambda sky:sky["height"])
+    sortedDays = sorted(days, key=lambda sky:sky["height"])
 
-    skyCopy = [-1]*(n_sky+1)
+    lastSky = n_sky
 
-    for sky in sortedSky:
-        skyCopy[sky["pos"]+1] = sky["height"]
+    for day in sortedDays:
+        if ((-day["height"]) < lastSky):
+            pos = findFirstSky((-day["height"])+1, lastSky)
+            for sky in range(pos, lastSky):
+                sky_pos = sortedSky[sky]["pos"]
+                adjacent = 0
+                newLand[sky_pos] = True
+                if ( sky_pos != 0):
+                    if (newLand[sky_pos-1]):
+                        adjacent += 1
+                if ( sky_pos != n_sky -1):
+                    if (newLand[sky_pos+1]):
+                        adjacent += 1
+                if (adjacent == 0):
+                    groups += 1
+                elif (adjacent  == 2):
+                    groups -= 1
+            lastSky = pos
 
-    days = [findFirstSky(int(day), n_sky) for day in input().split()]
-
+        groupList[day["pos"]] = str(groups)
+    print(" ".join(groupList))
     
-
-    # print(skyCopy)
-    # groups = 0
-    # for ind in range(1, n_sky):
-    #     if skyCopy(ind) != -1 and skyCopy(ind-1) == -1:
-    #         groups += 1
-    # results[test] = groups
-# print(results)
 
